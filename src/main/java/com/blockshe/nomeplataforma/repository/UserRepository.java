8f9091af7434ed2093186b66b5b27a4bec6784e5
aneskak <aneska.karina@hotmail.com>
2024-07-13 13:28:36 -0300

    add repository

--- src/main/java/com/blockshe/nomeplataforma/repository/UserRepository.java
+++ src/main/java/com/blockshe/nomeplataforma/repository/UserRepository.java
@@ -2,6 +2,18 @@ package com.blockshe.nomeplataforma.repository;
 
 import com.blockshe.nomeplataforma.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+@Repository //Indicando que a interface é um repositório
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findByEmail(String email);
+
+    public List<User> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
 }
+
+
